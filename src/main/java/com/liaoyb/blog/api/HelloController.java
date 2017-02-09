b1cfc6bd6cbce5d96792b446db509cddb9e7c493
OnlyYC <876714394@qq.com>
2017-02-10 00:04:05 +0800

    首页

--- src/main/java/com/liaoyb/blog/api/HelloController.java
+++ src/main/java/com/liaoyb/blog/api/HelloController.java
@@ -19,8 +19,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 	@RequestMapping("/")
-	@ResponseBody
 	public String index(){
-		return "hello";
+		return "index";
 	}
 }
