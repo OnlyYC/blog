d105ee18ecde56a3385846ab687efa20fcc678f0
OnlyYC <876714394@qq.com>
2017-02-19 00:23:09 +0800

    首页编辑

--- src/main/java/com/liaoyb/blog/controller/page/PageController.java
+++ src/main/java/com/liaoyb/blog/controller/page/PageController.java
@@ -3,13 +3,15 @@ package com.liaoyb.blog.controller.page;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 /**
  * @author ybliao2
  */
 @Controller
 public class PageController {
 	@RequestMapping("/")
-	public String index() {
+	public String index(Map map) {
 		return "index";
 	}
 
